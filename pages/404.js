a6a9c56cf93aea762e90dfca83e2cad165112812
Clive Stewart <clivestewart@hotmail.com>
2021-11-28 15:03:18 -0400

    fix &apos;

--- pages/404.js
+++ pages/404.js
@@ -11,7 +11,7 @@ const NotFound = () => {
 			<div className="container mx-auto py-16 h-full flex flex-col justify-center items-center space-y-12">
 				<div className="text-center space-y-6">
 					<h1 className="text-3xl sm:text-6xl"> 404 - Page not found</h1>
-					<p className="text-xl"> We can't the page you are looking for.</p>
+					<p className="text-xl"> We can&apos;t the page you are looking for.</p>
 				</div>
 				<Link href="/">
 					<a className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-md text-lg sm:text-xl focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50 whitespace-nowrap"> Go back home</a>
